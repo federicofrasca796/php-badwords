0d965bf8116807bdd7191fa83f4a4e948145ecfe
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-12-01 14:31:45 +0100

    add and display paragraph php variable

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+//Creo una variabile stringa
+$txt = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim eligendi non, delectus voluptatum ipsa dignissimos vero, illo ducimus culpa, quos amet ipsum dicta aliquam error dolore. Est enim nulla dolor.";
+//Mostro a schermo la variabile
+echo $txt . '<br>' . 'Il paragrafo è lungo ' . strlen($txt) . ' caratteri.';
+?>
